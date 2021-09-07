c35d26faf63d9ba020e8a23af6d6e35fceedeb67
BrockWeekley <48456042+BrockWeekley@users.noreply.github.com>
2021-09-07 10:52:45 -0500

    Remove cvtColor

--- rgb2gray.cpp
+++ rgb2gray.cpp
@@ -9,13 +9,30 @@ int main(int argc, char** argv )
         return -1;
     }
     cv::Mat image = cv::imread( argv[1], 1 );
+    int rows, cols;
+    rows = image.rows;
+    cols = image.cols;
+    
     cv::Mat grayImage;
+    grayImage.create(image.size(), CV_8UC1);
     if ( !image.data )
     {
         printf("No image data \n");
         return -1;
     }
-    cvtColor(image, grayImage, cv::COLOR_BGR2GRAY);
+    
+    for (int row = 0; row < rows; row++) {
+        const uchar* source = image.ptr<uchar>(row);
+        uchar* destination = grayImage.ptr<uchar>(row);
+        
+        for (int col = 0; col < cols; col++) {
+            // Blue Green Red
+            // 0.2989 * Red[x][y] + 0.5871 * Green[x][y] + 0.1140 * Blue[x][y]
+            
+            destination[col] = (uchar)(source[2] * 0.2989 + source[1] * 0.5871 + source[0] * 0.1140);
+            source += 3;
+        }
+    }
     cv::imwrite(argv[2], grayImage);
     cv::imshow("Display Image", grayImage);
     cv::waitKey(0);
